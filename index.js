d2a5885cfc15e796df412d18c6d91abddc0a3aa8
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-19 04:52:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ document.addEventListener("keydown", function(e) {
     let rightNumbers = dodger.style.right.replace("px", "");
     let right = parseInt(rightNumbers, 10);
 
-    dodger.style.right = `${right - 1}px`;
+    dodger.style.right = `${right + 1}px`;
   }
 });
 
